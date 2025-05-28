9f7d752a861c7d458a62628234d9bd2890655034
Aliffia Humaira <aliffiahumaira26@gmail.com>
2025-05-28 20:37:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,47 +15,47 @@ use Mews\Captcha\Captcha;
 |--------------------------------------------------------------------------
 */
 
-// Auth routes manual (login, register, logout)
+// Auth routes
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
-
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register']);
-
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
-// Home routes
-Route::get('/', [HomeController::class, 'index'])->name('index');
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/summaries', [HomeController::class, 'summary'])->name('summary');
-Route::get('/monthly/index', [HomeController::class, 'monthly'])->name('monthly.index');
+// Group routes that require login
+Route::middleware(['auth'])->group(function () {
+    // Home
+    Route::get('/', [HomeController::class, 'index'])->name('index');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/summaries', [HomeController::class, 'summary'])->name('summary');
+    Route::get('/monthly/index', [HomeController::class, 'monthly'])->name('monthly.index');
 
-// Notes Routes
-Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
-Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
-Route::post('/notes/store', [NoteController::class, 'store'])->name('notes.store');
-Route::get('/notes/edit/{id}', [NoteController::class, 'edit'])->name('notes.edit');
-Route::post('/notes/update', [NoteController::class, 'update'])->name('notes.update');
-Route::get('/notes/delete/{id}', [NoteController::class, 'destroy'])->name('notes.delete');
+    // Notes
+    Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
+    Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
+    Route::post('/notes/store', [NoteController::class, 'store'])->name('notes.store');
+    Route::get('/notes/edit/{id}', [NoteController::class, 'edit'])->name('notes.edit');
+    Route::post('/notes/update', [NoteController::class, 'update'])->name('notes.update');
+    Route::get('/notes/delete/{id}', [NoteController::class, 'destroy'])->name('notes.delete');
 
-// Income Routes
-Route::get('/incomes', [IncomeController::class, 'index'])->name('incomes.index');
-Route::get('/incomes/create', [IncomeController::class, 'create'])->name('incomes.create');
-Route::post('/incomes/store', [IncomeController::class, 'store'])->name('incomes.store');
-Route::get('/incomes/edit/{id}', [IncomeController::class, 'edit'])->name('incomes.edit');
-Route::post('/incomes/update', [IncomeController::class, 'update'])->name('incomes.update');
-Route::get('/incomes/delete/{id}', [IncomeController::class, 'destroy'])->name('incomes.delete');
+    // Income
+    Route::get('/incomes', [IncomeController::class, 'index'])->name('incomes.index');
+    Route::get('/incomes/create', [IncomeController::class, 'create'])->name('incomes.create');
+    Route::post('/incomes/store', [IncomeController::class, 'store'])->name('incomes.store');
+    Route::get('/incomes/edit/{id}', [IncomeController::class, 'edit'])->name('incomes.edit');
+    Route::post('/incomes/update', [IncomeController::class, 'update'])->name('incomes.update');
+    Route::get('/incomes/delete/{id}', [IncomeController::class, 'destroy'])->name('incomes.delete');
 
-// Expense Routes
-Route::get('/expense', [ExpenseController::class, 'index'])->name('expense.index');
-Route::get('/expense/create', [ExpenseController::class, 'create'])->name('expense.create');
-Route::post('/expense/store', [ExpenseController::class, 'store'])->name('expense.store');
-Route::get('/expense/edit/{id}', [ExpenseController::class, 'edit'])->name('expenses.edit'); // ini penting!
-Route::post('/expense/update', [ExpenseController::class, 'update'])->name('expenses.update');
-Route::get('/expense/delete/{id}', [ExpenseController::class, 'destroy'])->name('expenses.delete');
+    // Expense
+    Route::get('/expense', [ExpenseController::class, 'index'])->name('expense.index');
+    Route::get('/expense/create', [ExpenseController::class, 'create'])->name('expense.create');
+    Route::post('/expense/store', [ExpenseController::class, 'store'])->name('expense.store');
+    Route::get('/expense/edit/{id}', [ExpenseController::class, 'edit'])->name('expenses.edit');
+    Route::post('/expense/update', [ExpenseController::class, 'update'])->name('expenses.update');
+    Route::get('/expense/delete/{id}', [ExpenseController::class, 'destroy'])->name('expenses.delete');
+});
 
-
-
+// Captcha refresh
 Route::get('captcha', function () {
     return captcha_img('flat');
 });
